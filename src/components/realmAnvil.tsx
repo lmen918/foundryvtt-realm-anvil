4549a61abce126e42ce9d62c6b7f5e892c0cd243
LuisMendez-ACC <luis.mendez2@g.austincc.edu>
2025-02-25 20:38:24 -0600

    Snapshot of bare mimum for React TS.

--- src/components/realmAnvil.tsx
+++ src/components/realmAnvil.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import {ButtonComponent} from "./ButtonComponent";
 
 const RealmAnvil: React.FC = () => {
 
     return (
-        <div className="realmAnvil-Sidebar-content">
-            <h1>Realm Anvil</h1>
-            <p>An application to assist GMs and players with building the realm for their VTT system</p>
-        </div>
+        <>
+            <div className="realmAnvil-Sidebar-content">
+                <h1>Realm Anvil</h1>
+                <p>An application to assist GMs and players with building the realm for their VTT system</p>
+            </div>
+            <ButtonComponent />
+        </>
+
     )
 }
 export default RealmAnvil;
